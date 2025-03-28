6f6daf0e708970150b00fa58de7f4dd65cced749
Guilherme Costa <101594185+guihocosta@users.noreply.github.com>
2025-03-28 19:28:24 -0300

    Add files via upload

---
+++ Repositorio/Entidades/RepositorioCategoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio.Interfaces.Repositorio;
+using Microsoft.EntityFrameworkCore;
+using SistemaVenda.Dominio.Entidades;
+namespace Repositorio.Entidades
+{
+    public class RepositorioCategoria : Repositorio<Categoria>, IRepositorioCategoria
+    {
+        public RepositorioCategoria(DbContext dbContext) : base(dbContext){}
+    }
+}
---
+++ Repositorio/Repositorio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repositorio
+{
+    public abstract class Repositorio<TEntidade> : DbContext, IRepositorio<TEntidade> where
+        TEntidade : class, new()
+    {
+        DbContext Db;
+        DbSet<TEntidade> DbSetContext;
+
+        public Repositorio(DbContext dbContext)
+        {
+            Db = dbContext;
+            DbSetContext = Db.Set<TEntidade>();
+        }
+        public void Create(TEntidade Entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntidade Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntidade> Read()
+        {
+            return DbSetContext.AsNoTracking().ToList();
+        }
+    }
+}
